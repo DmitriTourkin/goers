5bdf0b369c309a730fbcd778453c68a23ae1d31e
Dmitrii Tourkin <ddiimmuuzz@yandex.ru>
2025-10-01 01:17:59 +0300

    exports, functions params start, package

--- hello.go
+++ hello.go
@@ -1,10 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"math"
 	"math/rand"
+	"fmt"
 )
+
+func ommitedTypeAdd(x, y int) int {
+	return x + y
+}
 
 func main() {
 	fmt.Println("My favourite number is ", rand.Intn(10))
+	fmt.Println("Exported Pi value ", math.Pi)
+	SayHello();
+	result := Add(5, 7)
+	fmt.Println("Result of Add Functin", result)
+	fmt.Println(ommitedTypeAdd(10, 5))
 }
